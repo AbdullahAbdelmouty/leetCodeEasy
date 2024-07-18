ebce7c1b491c77409ed300e0e8d8ac5d3608d0b1
abdallah abdelmoaty <abdallah.abdelmoaty@tasahel.local>
2024-07-18 17:26:11 +0300

    mergeTowLists start

--- leetCodeEasy.js
+++ leetCodeEasy.js
@@ -66,31 +66,87 @@
 //     return result
 // };
 // 13.Roman to Integer
-const input = "LVIII"
-// "MMCDXXV" = 1000 + 1000 + 100 + 500 + 10 + 10 + 5
-var romanToInt = function(s) {
-    let romanObj = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-    let arr = s.split("")
-    let integer = 0;
-    let prValue = 0;
-    for (let index = arr.length -1; index >= 0; index--) {
-        const value = romanObj[arr[index]];
-        if(value<prValue){
-            integer-=value
-        } else{
-            integer+=value;
-        }     
-        prValue = value
-    }
-    return integer;
-};
+// const input = "LVIII"
+// // "MMCDXXV" = 1000 + 1000 + 100 + 500 + 10 + 10 + 5
+// var romanToInt = function(s) {
+//     let romanObj = {
+//         'I': 1,
+//         'V': 5,
+//         'X': 10,
+//         'L': 50,
+//         'C': 100,
+//         'D': 500,
+//         'M': 1000
+//     }
+//     let arr = s.split("")
+//     let integer = 0;
+//     let prValue = 0;
+//     for (let index = arr.length -1; index >= 0; index--) {
+//         const value = romanObj[arr[index]];
+//         if(value<prValue){
+//             integer-=value
+//         } else{
+//             integer+=value;
+//         }     
+//         prValue = value
+//     }
+//     return integer;
+// };
 
-console.log(romanToInt(input));
+// console.log(romanToInt(input));
+
+// Merge Two Sorted Lists
+// 1- create linkedList
+const linkedList = class{
+    constructor(head = null){
+        this.head = head;
+    }
+}
+const ListNode = class{
+    constructor(data){
+        this.data = data;
+        this.next = null
+    }
+}
+const list1 = [1,2,4];
+const list2 = [1,3,4];
+var mergeTwoLists = function(list1, list2) {
+    let linked1 = convertArrToList(list1);
+    let linked2 = convertArrToList(list2);
+    let resultLinked = new linkedList();
+    let nodeOne = linked1.head;
+    let nodeTwo = linked2.head;
+    while(nodeOne&&nodeTwo){
+        console.log("#####");
+        if(nodeOne.data<nodeTwo.data){
+            console.log("one");
+        }
+        else{
+            console.log("two");
+        }
+        nodeOne = nodeOne.next;
+        nodeTwo = nodeTwo.next;
+    }
+};
+// convert Array To LinkedList
+const convertArrToList = (arr)=>{
+    let node = new ListNode(arr[0]);
+    let linked = new linkedList(node);
+    let node_ = linked.head;
+    for (let i = 1; i < arr.length; i++) {
+        node_.next = new ListNode(arr[i]);
+        node_ = node_.next;
+    }
+    return linked;
+}
+// Convert LinkedList To Array
+const convertListToArr = (list)=>{
+    let arr = [];
+    let node = list.head;
+    while(node){
+        arr.push(node.data)
+        node = node.next;
+    }
+    return arr;
+}
+mergeTwoLists(list1,list2)
